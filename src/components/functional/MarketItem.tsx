0f7d8b55875166de63633d7e9713fa16d000ba0c
Ayush Jain <ayushjain2205@gmail.com>
2024-09-06 19:31:45 +0530

    Fix market type error

--- src/components/functional/MarketItem.tsx
+++ src/components/functional/MarketItem.tsx
@@ -4,6 +4,7 @@ import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { PREDICTION_MARKET_ADDRESS } from "@/helpers/contractHelpers";
+import { Market } from "@/types";
 
 interface MarketItemProps {
   market: Market;
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface Market {
+  id: number;
+  creator: string;
+  question: string;
+  endTime: string;
+  resolved: boolean;
+  yesShares: string;
+  noShares: string;
+}
