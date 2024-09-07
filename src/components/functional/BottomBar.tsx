a0d79ac240144d611f55150805202fdd5640f49f
Ayush Jain <ayushjain2205@gmail.com>
2024-09-08 01:45:37 +0530

    Update bottom bar

--- src/components/functional/BottomBar.tsx
+++ src/components/functional/BottomBar.tsx
@@ -8,7 +8,7 @@ const BottomBar = () => {
   const menuItems = [
     { name: "Markets", path: "/markets", icon: BarChart2 },
     { name: "Live", path: "/live", icon: Tv },
-    { name: "Activity", path: "/activity", icon: ActivitySquare },
+    { name: "Create", path: "/create-market", icon: ActivitySquare },
   ];
 
   return (
